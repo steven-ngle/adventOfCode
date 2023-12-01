d354492e9328c15e4ac7bd3024f9878eb79ceeb7
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-12-01 22:02:49 +0100

    Day1 Part2

--- src/year2023/Day1.java
+++ src/year2023/Day1.java
@@ -1,12 +1,10 @@
 package year2023;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class Day1 {
 
